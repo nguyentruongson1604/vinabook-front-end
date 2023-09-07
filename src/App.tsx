3e2ff1c000fe6f28a7b91e2b1c5e4fc594155893
Sonvip <sonvip802@gmail.com>
2023-09-07 15:27:11 +0700

    master 9/7/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import HomePage from './components/pages/HomePage';
 import AuthorContent from './components/templates/AuthorContent';
+import BottomHeader from './components/templates/BottomHeader';
+import Nav from './components/templates/Nav';
+import TopHeader from './components/templates/TopHeader';
 function App() {
   return (
     <>
-      
-      <div >
+    <TopHeader/>
+    <BottomHeader/>
+    <Nav/>
       <AuthorContent/>
-      </div>
       
     </>
   )
